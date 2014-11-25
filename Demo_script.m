ed7bf77f94e5cd7db399c65b1bf3a9a003e526ce
Steven Huang <stevenhuang4321@gmail.com>
2014-11-25 23:45:42 +0800

    Fisrt Commit

---
+++ Demo_script.m
@@ -0,0 +1,27 @@
+clear
+close all
+load('f_0.mat');
+para.T_min =0; % lower threshold of ring artifacts
+para.T_max =0.8; % higher threshold of ring artifacts
+para.M_rad = 40 ; % empirical filter width
+ring_center.x0 =275; % x coordinate of ring artifact center.
+ring_center.y0 = 303;% y coordinate of ring artifact center.
+[ f_corr ,delta_f] = ring_artifact_removal( f_0,para,ring_center );
+
+figure,%Line profile
+plot(f_0(end/2,:),'go')
+hold on
+plot(f_corr(end/2,:),'r*')
+title('Line profile')
+hold off
+
+figure,
+a = subplot(1,2,1);
+
+imshow(f_0);
+title(a,'Original image');
+
+b = subplot(1,2,2);
+
+imshow(f_corr);
+title(b,'Image with ring artifact removal');
---
+++ ring_artifact_removal.m
@@ -0,0 +1,81 @@
+function [ f_corr ,delta_f] = ring_artifact_removal( f_0,para,ring_center )
+%RING_ARTIFACT_REMOVAL This function is used to remove the ring artifacts of reconstructed image. It is based
+%                        on radial median filtering in cartesian coordinates(RCC).
+% usuage:
+%  [f_corr ,delta_f] = ring_artifact_removal( f_0,T_max,T_min,ring_center )
+% where:
+%   f_0: denotes the original image with ring artifacts.
+%   T_max: denotes the upper threshold of ring artifacts value.
+%   T_min: denote the lower threshold of ring artifacts value.
+%   ring_center.x0/.y0: denote the ring artifact center coordinates.
+%   f_corr : denote the final image without ring artifacts.
+%   delta_f: denote ring artifact image.
+% Reference:
+   % Prell, D., et al. (2009). "Comparison of ring artifact correction methods for flat-detector CT." 
+   %   Physics in Medicine and Biology 54(12): 3881.
+[Iwidth, Iheight] = size(f_0);
+f_thres = zeros(Iwidth, Iheight);
+% delta_f = f_thres;
+f_M = f_thres;
+f_corr = f_thres;
+
+count = 0;
+for i = 1:Iwidth*Iheight
+    if (f_0(i) < para.T_max)&&(f_0(i)>para.T_min)
+        f_thres(i) = f_0(i);
+        count = count +1;
+    end
+end
+
+
+x0 = ring_center.x0;
+y0 = ring_center.y0;
+RF= 0.5;%radius of the focus trajectory
+RD = 0.5;%radius of the detector trajectory
+du = 0.1;
+dv = 0.1;
+dx = 0.1;
+dy = 0.1;
+duv = (du+dv)/2;% mean detector pixel size.
+dxy = (dx+dy)/2;%mean image pixel size.
+b = 1; %pixel-binning factor.
+dRA =(RF/(RD+RF))*duv*b/dxy;%circular artifact size.
+
+xs = zeros(2*para.M_rad+1,1);
+ys = xs;
+fs = xs;
+% processNo = 1;
+% f_0(257,294)=0;
+for x=1:Iwidth
+    for y = 1:Iheight
+        if((f_0(x,y)~=0)&&~((x==x0)&&(y == y0)))
+%             sct =sprintf('x=%d,y=%d',x,y);
+%             disp(sct);
+            dx = x - x0;
+            dy = y - y0;
+            dR = sqrt(dx*dx+dy*dy);
+            dxe = dx/dR;
+            dye = dy/dR;
+            for n = 1:2*para.M_rad+1
+                xs(n) = round(x0+(dR - n*dRA)*dxe);
+                ys(n) = round(y0+(dR - n*dRA)*dye);
+                fs(n) = f_0(xs(n),ys(n));
+            end
+            sort_fs = sort(fs);
+            mean_fs = sort_fs(para.M_rad+1); % get median value.
+            f_M(x, y) = mean_fs;
+%             sct =sprintf('succeed x=%d,y=%d,count=%d.',x,y,processNo);
+%             processNo = processNo +1;
+%             disp(sct);
+        end
+        
+    end
+end
+delta_f = f_thres - f_M;
+f_corr = f_0 - delta_f;
+
+end
+
+
+
+
